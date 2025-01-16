1e626be699e296a0c98713b85392969ceae79617
Alexander mena <alexismena-90@hotmail.com>
2025-01-16 15:47:14 -0800

    adding the basics

---
+++ React Portfolio/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+function Nav() {
+  return (
+    <div className="nav-scroller py-1 mb-3 border-bottom">
+      <nav className="nav nav-underline justify-content-between">
+        <a className="nave-item nav-link link-body-emphasis" href="#">
+          Testing
+        </a>
+        <a className="nave-item nav-link link-body-emphasis" href="#">
+          Testing
+        </a>
+        <a className="nave-item nav-link link-body-emphasis" href="#">
+          Testing
+        </a>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
