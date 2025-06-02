7d308757330aeaf98cff75e11ae041f793ee5a5a
Alexander mena <alexismena-90@hotmail.com>
2025-06-02 11:43:20 -0700

    Cleaned up about me

--- React Portfolio/src/Pages/Aboutme.jsx
+++ React Portfolio/src/Pages/Aboutme.jsx
@@ -13,23 +13,20 @@ function Aboutme() {
         <div className="row featurette">
           <div className="col-md-7 order-md-2">
             <h2 className="featurette-heading">
-              Hello everyone my name is Alexander!
-              <span className="text-muted">Story time!.</span>
+              Hello, my name is Alexander !
             </h2>
             <p className="lead">
-              I am born and raised in San Francisco. All my life i have been
-              around tech and i seem to be really good at learning and
-              understanding how things worked. I started my journey when i was
-              13 years old and i got my first iPhone. This iPhone 3Gs had a
-              password and was carrier locked to At&t. I then found a way to
-              restore the device, than jailbreak the device. Witch allowed me to
-              activate the device and carrier unlock the device, so i could use
-              the iPhone with any GSM carrier. At that time the iPhone was only
-              sold to use under the At&t network. At the age of 13, i started
-              working at a family owned cellphone repair shop. Became a
-              cellphone tech, and continued learning more about all forms of
-              tech. Now i found the amazing world of coding 🚀. Lets see where
-              this takes me now!
+              Born and raised in San Francisco. All my life i have been around
+              tech and i seem to be really good at learning and understanding
+              how things worked. I started my journey when i was 13 years old
+              and i got my first iPhone. This iPhone 3Gs had a password and was
+              carrier locked to AT&T. I then found a way to restore the device
+              and then jailbreak the device this allowed me to unlock the device
+              so i could use the iPhone with any GSM carrier. At that time the
+              iPhone was only sold to use under the AT&T network. At the age of
+              13, i started working at a family owned cellphone repair shop.
+              Became a cellphone tech, and continued learning more about all
+              forms of tech. Now i found the amazing world of coding 🚀.
             </p>
           </div>
           <div className="col-md-5 order-md-1">
