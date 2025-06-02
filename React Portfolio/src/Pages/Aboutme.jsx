af06bc94068c51c6ddafc92ccbf5044fc658d009
Alexander mena <alexismena-90@hotmail.com>
2025-06-02 12:29:16 -0700

    Cleaned up about m3

--- React Portfolio/src/Pages/Aboutme.jsx
+++ React Portfolio/src/Pages/Aboutme.jsx
@@ -16,17 +16,13 @@ function Aboutme() {
               Hello, my name is Alexander !
             </h2>
             <p className="lead">
-              Born and raised in San Francisco. All my life i have been around
-              tech and i seem to be really good at learning and understanding
-              how things worked. I started my journey when i was 13 years old
-              and i got my first iPhone. This iPhone 3Gs had a password and was
-              carrier locked to AT&T. I then found a way to restore the device
-              and then jailbreak the device this allowed me to unlock the device
-              so i could use the iPhone with any GSM carrier. At that time the
-              iPhone was only sold to use under the AT&T network. At the age of
-              13, i started working at a family owned cellphone repair shop.
-              Became a cellphone tech, and continued learning more about all
-              forms of tech. Now i found the amazing world of coding 🚀.
+              Born and raised in San Francisco. I'm currently an operator at
+              Compupod. I love developing apps and websites. I'm a highly
+              motivated individual with a great understanding of both Front end
+              development and Back end development. The languages I've worked
+              with are HTML, CSS, JavaScript, NodeJs, React and more. I am
+              wanting to continue to learn more languages in this field. I'm a
+              team player and also enjoy working until the job is done.
             </p>
           </div>
           <div className="col-md-5 order-md-1">
